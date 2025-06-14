2fffae8c38549376ed9310290af306dec0257b65
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2025-06-14 18:03:33 +0200

    feat(cmd/list): add JSON output format

--- cmd/list_cmd.go
+++ cmd/list_cmd.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
+	"io"
+
+	"dario.cat/mergo"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 
@@ -16,17 +20,41 @@ var listCmd = &cobra.Command{
 	RunE:        runList,
 }
 
+// Output format constants
+const (
+	OutputFormatText = "text"
+	OutputFormatJSON = "json"
+)
+
+var listCmdFlags = listOptions{
+	OutputFormat: "",
+}
+
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.Flags().StringVarP(&listCmdFlags.OutputFormat, "output", "o", OutputFormatText, "Output format: text|json")
 }
 
 type listOptions struct {
-	Roots []string
+	Roots        []string
+	OutputFormat string
+}
+
+type repoInfo struct {
+	Path    string `json:"path"`
+	URL     string `json:"url"`
+	Branch  string `json:"branch,omitempty"`
+	IsClean bool   `json:"is_clean,omitempty"`
 }
 
 func runList(cmd *cobra.Command, args []string) error {
 	opts, err := parseListArgs(args)
 	if err != nil {
+		return err
+	}
+
+	if err := mergo.Merge(&opts, listCmdFlags); err != nil {
 		return err
 	}
 
@@ -45,16 +73,35 @@ func runList(cmd *cobra.Command, args []string) error {
 
 	spinner.Stop() //nolint:errcheck
 
+	var repos []repoInfo
+
 	for it := repoPaths.Iterator(); it.HasNext(); {
 		node, _ := it.Next()
 		repoPath := string(node.Key())
 
-		project, _, _, err := gitProjectInfo(repoPath)
+		project, url, ref, err := gitProjectInfo(repoPath)
 		if err != nil {
 			return err
 		}
 
-		pterm.Println(project)
+		if opts.OutputFormat == OutputFormatJSON {
+			branch := ""
+			if ref != nil {
+				branch = ref.Name().Short()
+			}
+
+			repos = append(repos, repoInfo{
+				Path:   project,
+				URL:    url,
+				Branch: branch,
+			})
+		} else {
+			pterm.Println(project)
+		}
+	}
+
+	if opts.OutputFormat == OutputFormatJSON {
+		return outputJSON(cmd.OutOrStdout(), repos)
 	}
 
 	return nil
@@ -78,4 +125,10 @@ func parseListArgs(args []string) (listOptions, error) {
 	}
 
 	return opts, nil
+}
+
+func outputJSON(w io.Writer, v interface{}) error {
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(v)
 }
